48592411a7849f93d33185376d643db28d62c43d
Dimitris Charalampidopoulos <dcharalab@gmail.com>
2022-02-25 15:35:13 +0200

    css

--- pages/index.vue
+++ pages/index.vue
@@ -3,16 +3,6 @@
 </template>
 
 <style>
-@import url("https://fonts.googleapis.com/css2?family=ZCOOL+QingKe+HuangYou&display=swap");
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-  font-family: "ZCOOL QingKe HuangYou", cursive;
-}
 #list-item {
   list-style-type: none;
 }
