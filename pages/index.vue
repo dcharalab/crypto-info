bd1f19562398e28afc7b2b17402bafa5db73aeb9
Dimitris Charalampidopoulos <dcharalab@gmail.com>
2022-02-25 16:29:05 +0200

    css update

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,14 @@
 <template>
-  <coinListVue />
+  <div class="center-screen">
+    <coinListVue />
+  </div>
 </template>
 
 <style>
 #list-item {
   list-style-type: none;
+}
+.center-screen {
+  position: relative;
 }
 </style>
